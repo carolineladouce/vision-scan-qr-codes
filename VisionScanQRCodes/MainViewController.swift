ef29f244b3d2d8f3a8fb6b1728ec48acd077b808
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-12 15:29:33 -0500

    Project captures live back camera feed and displays it to the screen

--- VisionScanQRCodes/MainViewController.swift
+++ VisionScanQRCodes/MainViewController.swift
@@ -10,14 +10,44 @@ import AVFoundation
 import Vision
 
 class MainViewController: UIViewController {
-
+    
+    private let captureSession = AVCaptureSession()
+    
+    private func addCamerInput() {
+        let device = AVCaptureDevice.default(for: .video)!
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let preview = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        preview.videoGravity = .resizeAspect
+        return preview
+    }()
+    
+    private func addPreviewLayer() {
+        self.view.layer.addSublayer(self.previewLayer)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         view.backgroundColor = .white
+        
+        self.addCamerInput()
+        self.addPreviewLayer()
+        self.captureSession.startRunning()
+        
+        
     }
-
-
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.view.bounds
+    }
+    
 }
 
