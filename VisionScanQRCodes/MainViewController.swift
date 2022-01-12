a1da94e2448b21a7ee8e67caa301024c438a3a7f
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-12 15:05:37 -0500

    Initial Commit

---
+++ VisionScanQRCodes/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VisionScanQRCodes
+//
+//  Created by Caroline LaDouce on 1/12/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
